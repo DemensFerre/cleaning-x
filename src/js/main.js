c6f4dd9aa314387554c1ade4866bef4cbb6ea17d
Max <87299631+DemensFerre@users.noreply.github.com>
2023-01-02 11:18:51 +0300

    added sticky header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
+const scrollHeader = () => {
+  const header = document.querySelector(".header");
 
+  window.addEventListener("scroll", () => {
+    if (window.scrollY >= 100) {
+      header.classList.add("header_scroll");
+    } else {
+      header.classList.remove("header_scroll");
+    }
+  });
+};
+
+scrollHeader();
